142b782b5981f33d6708823b2e75bd0829ddeaee
EunHyeok <secun77@naver.com>
2025-06-26 15:20:33 +0900

    App.tsx 오류  수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -23,7 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/adoption" element={<AdoptionPage />} />
-          <Route path="/adoption/create" element={<AdoptionCreate />} />
+          {/* <Route path="/adoption/create" element={<AdoptionCreate />} /> */}
           <Route path="/adoption-detail/:id" element={<AdoptionDetail />} />
           <Route path="/rescue-detail/:id" element={<RescueDetail />} />
           <Route path="*" element={<NotFound />} />
